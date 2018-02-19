256f749adde66e7d97de2cb191df0990134891c9
Oleg Butuzov <butuzov@made.ua>
2018-02-19 08:20:36 +0200

    Added `Arrays` translation

--- examples/arrays/arrays.go
+++ examples/arrays/arrays.go
@@ -1,37 +1,36 @@
-// In Go, an _array_ is a numbered sequence of elements of a
-// specific length.
-
+// В Go, _массив_ це числова послідовність елементів визначеної
+// довжини.
 package main
 
 import "fmt"
 
 func main() {
 
-    // Here we create an array `a` that will hold exactly
-    // 5 `int`s. The type of elements and length are both
-    // part of the array's type. By default an array is
-    // zero-valued, which for `int`s means `0`s.
+    // Ось ми створюємо масив `a` що буде міститити рівно
+    // 5 `int`ів. Тип елементів та довжина обидвоє частиною
+    // типу масиву. По замовчуванню масив скаладється з нульових
+    // значень, що для цілих чисел (`int`ів) означає значення `0`.
     var a [5]int
     fmt.Println("emp:", a)
 
-    // We can set a value at an index using the
-    // `array[index] = value` syntax, and get a value with
-    // `array[index]`.
+    // Доступ за індексом дозволяє нам встановлювати значення
+    // використовуючи насутпний синтакс `array[index] = value` та
+    // повертати значення за допомогою `array[index]`.
     a[4] = 100
     fmt.Println("set:", a)
     fmt.Println("get:", a[4])
 
-    // The builtin `len` returns the length of an array.
+    // Вбудована фукнція `len` поверне довжину масиву.
     fmt.Println("len:", len(a))
 
-    // Use this syntax to declare and initialize an array
-    // in one line.
+    // Використовуйте наступний синтаксис для декларації
+    // та ініціалізації масиву в одну строку.
     b := [5]int{1, 2, 3, 4, 5}
     fmt.Println("dcl:", b)
 
-    // Array types are one-dimensional, but you can
-    // compose types to build multi-dimensional data
-    // structures.
+    // Масиви самі по собі є одномірними, але ви можете
+    // компонувати типи і будувати башато мірні структури
+    // данних
     var twoD [2][3]int
     for i := 0; i < 2; i++ {
         for j := 0; j < 3; j++ {
