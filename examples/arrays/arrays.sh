8ebf14b4e6b48a25fcca601a2c6cad35788df3d3
Oleg Butuzov <butuzov@made.ua>
2018-02-19 12:50:21 +0200

    Array: example updated
    + spelling
    + additional example

--- examples/arrays/arrays.sh
+++ examples/arrays/arrays.sh
@@ -1,12 +1,13 @@
-# Note that arrays appear in the form `[v1 v2 v3 ...]`
-# when printed with `fmt.Println`.
+# Зауважте, масив що надруковано за допомогою
+# `fmt.Println` буде виведено у вигляді `[v1 v2 v3 ...]`
 $ go run arrays.go
 emp: [0 0 0 0 0]
 set: [0 0 0 0 100]
 get: 100
 len: 5
 dcl: [1 2 3 4 5]
+elipsis: [0 1 2 4 8 16]
 2d:  [[0 1 2] [1 2 3]]
 
-# You'll see _slices_ much more often than arrays in
-# typical Go. We'll look at slices next.
+# В Go частіше використовуватимуться _зрізи_ (_slices_)
+# З ними ми познамомимось в насутпному прикладі.
