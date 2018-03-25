15fa659bb9d2a2b49d657c5c3b0d5cee05202c06
Oleg Butuzov <butuzov@made.ua>
2018-03-25 19:06:21 +0300

    Working on deploy 06
    deploy.sh update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
+GH="github.com/butuzov/gobyexample"
 
 echo -e "Deploying translations to GitHub"
-DIR=$HOME/gopath/src/github.com/butuzov/gobyexample
-cd $DIR
-ls -lah
+rm -rf public/.*
+cd public
+
+git config --global user.email "butuzov@made.ua"
+git config --global user.name "Oleg Butuzov"
+git init
+git remote add -f origin "https://$GH"
+git checkout gobyexample.com.ua
+
+cd .. && tools/build && cd public
+
+# ls -lah
+echo -e "Sources Normalization"
+sed -i.back s/class=\"translated\"//g index.html
+sed -i.back s/list-style:decimal\;//g site.css
+unlink index.html.back
+unlink site.css.back
+
+COMMIT=$(git status | grep "nothing to commit, working tree clean")
+if [[ -z $COMMIT ]]; then
+	echo "Updating pages @ gobyexample.com.ua branch"
+	git add --all
+	git commit -m "Travis CI | ${MSG}"
+	git push "https://butuzov:${GITHUB_TOKEN}@${GH}" gobyexample.com.ua
+fi
