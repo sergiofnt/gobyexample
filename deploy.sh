b2b7c4fde1d39b29b117057c869aeb568455218e
Oleg Butuzov <butuzov@made.ua>
2018-03-25 18:37:51 +0300

    Working on deploy 05
    One more Directory Test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo -e "Deploying translations to GitHub"
-cd $HOME/gopath/src/github.com/butuzov/gobyexample/public
+DIR=$HOME/gopath/src/github.com/butuzov/gobyexample
+cd $DIR
 ls -lah
