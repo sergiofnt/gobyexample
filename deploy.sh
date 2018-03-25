d60b9ff7b5f80ae0d3f0a3564ad5ef0267f72746
Oleg Butuzov <butuzov@made.ua>
2018-03-25 19:12:20 +0300

    Working on deploy 07
    hope its final checks

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,19 @@
 GH="github.com/butuzov/gobyexample"
 
 echo -e "Deploying translations to GitHub"
-rm -rf public/.*
+
+cd $TRAVIS_BUILD_DIR
+# clean up
+rm -f public/*
+
+# generate new files.
 cd public
 
 git config --global user.email "butuzov@made.ua"
 git config --global user.name "Oleg Butuzov"
 git init
 git remote add -f origin "https://$GH"
-git checkout gobyexample.com.ua
+git checkout -f gobyexample.com.ua
 
 cd .. && tools/build && cd public
 
@@ -20,10 +25,16 @@ sed -i.back s/list-style:decimal\;//g site.css
 unlink index.html.back
 unlink site.css.back
 
+echo -e "Norm Test"
+ls -lah
+
 COMMIT=$(git status | grep "nothing to commit, working tree clean")
+
+echo $COMMIT
+
 if [[ -z $COMMIT ]]; then
 	echo "Updating pages @ gobyexample.com.ua branch"
 	git add --all
 	git commit -m "Travis CI | ${MSG}"
-	git push "https://butuzov:${GITHUB_TOKEN}@${GH}" gobyexample.com.ua
+	git push --dry-run "https://${GITHUB_TOKEN}@${GH}" gobyexample.com.ua
 fi
