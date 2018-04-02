6465933edf50776363349bcbac456af0fec263b1
Oleg Butuzov <butuzov@made.ua>
2018-04-02 10:59:22 +0300

    Time Formatting example translated

--- examples/time-formatting-parsing/time-formatting-parsing.go
+++ examples/time-formatting-parsing/time-formatting-parsing.go
@@ -1,5 +1,5 @@
-// Go supports time formatting and parsing via
-// pattern-based layouts.
+// Підтримка форматування та розбору (з рядків) часу
+// в Go організована через шаблонізовані рядкові літерали.
 
 package main
 
@@ -7,44 +7,45 @@ import "fmt"
 import "time"
 
 func main() {
-    p := fmt.Println
+	p := fmt.Println
 
-    // Here's a basic example of formatting a time
-    // according to RFC3339, using the corresponding layout
-    // constant.
-    t := time.Now()
-    p(t.Format(time.RFC3339))
+	// Це простий приклад форматування часу згідно до RFC3339
+	// використовуючи відповідну часову константу.
+	t := time.Now()
+	p(t.Format(time.RFC3339))
 
-    // Time parsing uses the same layout values as `Format`.
-    t1, e := time.Parse(
-        time.RFC3339,
-        "2012-11-01T22:08:41+00:00")
-    p(t1)
+	// Парсинг рядку в час ивкористовує теж значення `вигляду`,
+	// що і `Format`.
+	t1, e := time.Parse(
+		time.RFC3339,
+		"2012-11-01T22:08:41+00:00")
+	p(t1)
 
-    // `Format` and `Parse` use example-based layouts. Usually
-    // you'll use a constant from `time` for these layouts, but
-    // you can also supply custom layouts. Layouts must use the
-    // reference time `Mon Jan 2 15:04:05 MST 2006` to show the
-    // pattern with which to format/parse a given time/string.
-    // The example time must be exactly as shown: the year 2006,
-    // 15 for the hour, Monday for the day of the week, etc.
-    p(t.Format("3:04PM"))
-    p(t.Format("Mon Jan _2 15:04:05 2006"))
-    p(t.Format("2006-01-02T15:04:05.999999-07:00"))
-    form := "3 04 PM"
-    t2, e := time.Parse(form, "8 41 PM")
-    p(t2)
+	// І `Format`, і `Parse` використовують для розпізнавання часу
+	// строки з прикладами. Зазвичай ви будете використовувати
+	// вже готові константи пакету `time` але, ви можете створювати свої власні
+	// шаблони для розпізнання часу. Такі шаблони повинні використовувати
+	// посилання на час `Mon Jan 2 15:04:05 MST 2006` щоб "відтворити" шаблон
+	// за яким час можна розпізнати/відформатувати у відповідний рядок/час.
+	// Приклад часу мусить бути таким як показано: рік 2006,
+	// 15 для годни, Monday відповідно для дня тижня, ну і так далі.
+	p(t.Format("3:04PM"))
+	p(t.Format("Mon Jan _2 15:04:05 2006"))
+	p(t.Format("2006-01-02T15:04:05.999999-07:00"))
+	form := "3 04 PM"
+	t2, e := time.Parse(form, "8 41 PM")
+	p(t2)
 
-    // For purely numeric representations you can also
-    // use standard string formatting with the extracted
-    // components of the time value.
-    fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
-        t.Year(), t.Month(), t.Day(),
-        t.Hour(), t.Minute(), t.Second())
+	// Чисто для числового представлення ви також можете
+	// використовувати стандартне рядкове форматування
+	// за допомогою доступних компонентів значення часу.
+	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
 
-    // `Parse` will return an error on malformed input
-    // explaining the parsing problem.
-    ansic := "Mon Jan _2 15:04:05 2006"
-    _, e = time.Parse(ansic, "8:41PM")
-    p(e)
+	// `Parse` поверне помилку (якщо вхідна змінна деформована)
+	// з поясненням в чому виникла проблема.
+	ansic := "Mon Jan _2 15:04:05 2006"
+	_, e = time.Parse(ansic, "8:41PM")
+	p(e)
 }
