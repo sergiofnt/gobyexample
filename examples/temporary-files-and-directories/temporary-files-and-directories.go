73bd06dc32d11015a53baf05d6d7ca542a79fd6e
Oleg Butuzov <butuzov@made.ua>
2019-09-30 17:35:52 +0300

    misc updates
    + fixed markdown link to `mutexes` page
    + sources saved with spaces (after html) generated
    + fixed link to license and repository (from homepage)

--- examples/temporary-files-and-directories/temporary-files-and-directories.go
+++ examples/temporary-files-and-directories/temporary-files-and-directories.go
@@ -7,59 +7,59 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
+    "fmt"
+    "io/ioutil"
+    "os"
+    "path/filepath"
 )
 
 func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+    if e != nil {
+        panic(e)
+    }
 }
 
 func main() {
 
-	// The easiest way to create a temporary file is by
-	// calling `ioutil.TempFile`. It creates a file *and*
-	// opens it for reading and writing. We provide `""`
-	// as the first argument, so `ioutil.TempFile` will
-	// create the file in the default location for our OS.
-	f, err := ioutil.TempFile("", "sample")
-	check(err)
+    // The easiest way to create a temporary file is by
+    // calling `ioutil.TempFile`. It creates a file *and*
+    // opens it for reading and writing. We provide `""`
+    // as the first argument, so `ioutil.TempFile` will
+    // create the file in the default location for our OS.
+    f, err := ioutil.TempFile("", "sample")
+    check(err)
 
-	// Display the name of the temporary file. On
-	// Unix-based OSes the directory will likely be `/tmp`.
-	// The file name starts with the prefix given as the
-	// second argument to `ioutil.TempFile` and the rest
-	// is chosen automatically to ensure that concurrent
-	// calls will always create different file names.
-	fmt.Println("Temp file name:", f.Name())
+    // Display the name of the temporary file. On
+    // Unix-based OSes the directory will likely be `/tmp`.
+    // The file name starts with the prefix given as the
+    // second argument to `ioutil.TempFile` and the rest
+    // is chosen automatically to ensure that concurrent
+    // calls will always create different file names.
+    fmt.Println("Temp file name:", f.Name())
 
-	// Clean up the file after we're done. The OS is
-	// likely to clean up temporary files by itself after
-	// some time, but it's good practice to do this
-	// explicitly.
-	defer os.Remove(f.Name())
+    // Clean up the file after we're done. The OS is
+    // likely to clean up temporary files by itself after
+    // some time, but it's good practice to do this
+    // explicitly.
+    defer os.Remove(f.Name())
 
-	// We can write some data to the file.
-	_, err = f.Write([]byte{1, 2, 3, 4})
-	check(err)
+    // We can write some data to the file.
+    _, err = f.Write([]byte{1, 2, 3, 4})
+    check(err)
 
-	// If we intend to write many temporary files, we may
-	// prefer to create a temporary *directory*.
-	// `ioutil.TempDir`'s arguments are the same as
-	// `TempFile`'s, but it returns a directory *name*
-	// rather than an open file.
-	dname, err := ioutil.TempDir("", "sampledir")
-	fmt.Println("Temp dir name:", dname)
+    // If we intend to write many temporary files, we may
+    // prefer to create a temporary *directory*.
+    // `ioutil.TempDir`'s arguments are the same as
+    // `TempFile`'s, but it returns a directory *name*
+    // rather than an open file.
+    dname, err := ioutil.TempDir("", "sampledir")
+    fmt.Println("Temp dir name:", dname)
 
-	defer os.RemoveAll(dname)
+    defer os.RemoveAll(dname)
 
-	// Now we can synthesize temporary file names by
-	// prefixing them with our temporary directory.
-	fname := filepath.Join(dname, "file1")
-	err = ioutil.WriteFile(fname, []byte{1, 2}, 0666)
-	check(err)
+    // Now we can synthesize temporary file names by
+    // prefixing them with our temporary directory.
+    fname := filepath.Join(dname, "file1")
+    err = ioutil.WriteFile(fname, []byte{1, 2}, 0666)
+    check(err)
 }
--- examples/xml/xml.go
+++ examples/xml/xml.go
@@ -4,8 +4,8 @@
 package main
 
 import (
-	"encoding/xml"
-	"fmt"
+    "encoding/xml"
+    "fmt"
 )
 
 // This type will be mapped to XML. Similarly to the
@@ -16,54 +16,54 @@ import (
 // `id,attr` means that the `Id` field is an XML
 // _attribute_ rather than a nested element.
 type Plant struct {
-	XMLName xml.Name `xml:"plant"`
-	Id      int      `xml:"id,attr"`
-	Name    string   `xml:"name"`
-	Origin  []string `xml:"origin"`
+    XMLName xml.Name `xml:"plant"`
+    Id      int      `xml:"id,attr"`
+    Name    string   `xml:"name"`
+    Origin  []string `xml:"origin"`
 }
 
 func (p Plant) String() string {
-	return fmt.Sprintf("Plant id=%v, name=%v, origin=%v",
-		p.Id, p.Name, p.Origin)
+    return fmt.Sprintf("Plant id=%v, name=%v, origin=%v",
+        p.Id, p.Name, p.Origin)
 }
 
 func main() {
-	coffee := &Plant{Id: 27, Name: "Coffee"}
-	coffee.Origin = []string{"Ethiopia", "Brazil"}
+    coffee := &Plant{Id: 27, Name: "Coffee"}
+    coffee.Origin = []string{"Ethiopia", "Brazil"}
 
-	// Emit XML representing our plant; using
-	// `MarshalIndent` to produce a more
-	// human-readable output.
-	out, _ := xml.MarshalIndent(coffee, " ", "  ")
-	fmt.Println(string(out))
+    // Emit XML representing our plant; using
+    // `MarshalIndent` to produce a more
+    // human-readable output.
+    out, _ := xml.MarshalIndent(coffee, " ", "  ")
+    fmt.Println(string(out))
 
-	// To add a generic XML header to the output, append
-	// it explicitly.
-	fmt.Println(xml.Header + string(out))
+    // To add a generic XML header to the output, append
+    // it explicitly.
+    fmt.Println(xml.Header + string(out))
 
-	// Use `Unmarhshal` to parse a stream of bytes with XML
-	// into a data structure. If the XML is malformed or
-	// cannot be mapped onto Plant, a descriptive error
-	// will be returned.
-	var p Plant
-	if err := xml.Unmarshal(out, &p); err != nil {
-		panic(err)
-	}
-	fmt.Println(p)
+    // Use `Unmarhshal` to parse a stream of bytes with XML
+    // into a data structure. If the XML is malformed or
+    // cannot be mapped onto Plant, a descriptive error
+    // will be returned.
+    var p Plant
+    if err := xml.Unmarshal(out, &p); err != nil {
+        panic(err)
+    }
+    fmt.Println(p)
 
-	tomato := &Plant{Id: 81, Name: "Tomato"}
-	tomato.Origin = []string{"Mexico", "California"}
+    tomato := &Plant{Id: 81, Name: "Tomato"}
+    tomato.Origin = []string{"Mexico", "California"}
 
-	// The `parent>child>plant` field tag tells the encoder
-	// to nest all `plant`s under `<parent><child>...`
-	type Nesting struct {
-		XMLName xml.Name `xml:"nesting"`
-		Plants  []*Plant `xml:"parent>child>plant"`
-	}
+    // The `parent>child>plant` field tag tells the encoder
+    // to nest all `plant`s under `<parent><child>...`
+    type Nesting struct {
+        XMLName xml.Name `xml:"nesting"`
+        Plants  []*Plant `xml:"parent>child>plant"`
+    }
 
-	nesting := &Nesting{}
-	nesting.Plants = []*Plant{coffee, tomato}
+    nesting := &Nesting{}
+    nesting.Plants = []*Plant{coffee, tomato}
 
-	out, _ = xml.MarshalIndent(nesting, " ", "  ")
-	fmt.Println(string(out))
+    out, _ = xml.MarshalIndent(nesting, " ", "  ")
+    fmt.Println(string(out))
 }
